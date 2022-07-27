adc98ff1c94386b09acd7533293242932bb6658d
steam88ys <yssteam88@gmail.com>
2022-07-27 15:37:32 +0900

    명시적 Intent 데이터 전달 완성

--- app/src/main/java/kr/hs/emirim/s2102/mirim_project_0727_03/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2102/mirim_project_0727_03/MainActivity.java
@@ -2,13 +2,42 @@ package kr.hs.emirim.s2102.mirim_project_0727_03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+    RadioGroup rg;
+    EditText editName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rg = findViewById(R.id.rg);
+        editName = findViewById(R.id.edit_title);
+        Button btnStart = findViewById(R.id.btn_start);
+        btnStart.setOnClickListener(btnStartListener);
     }
+
+    View.OnClickListener btnStartListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Class c = null;
+            switch (rg.getCheckedRadioButtonId()) {
+                case R.id.rb_second:
+                    c = SecondActivity.class;
+                    break;
+                case R.id.rb_third:
+                    c = ThirdActivity.class;
+                    break;
+            }
+            Intent intent = new Intent(getApplicationContext(), c);
+            intent.putExtra("title", editName.getText().toString());
+            startActivity(intent);
+        }
+    };
 }
---
+++ app/src/main/java/kr/hs/emirim/s2102/mirim_project_0727_03/SecondActivity.java
@@ -0,0 +1,31 @@
+package kr.hs.emirim.s2102.mirim_project_0727_03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        TextView textTitle = findViewById(R.id.text_title);
+        textTitle.setText(title);
+        Button btnPrev = findViewById(R.id.btn_prev);
+        btnPrev.setOnClickListener(btnPrevListener);
+    }
+
+    View.OnClickListener btnPrevListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            finish();
+        }
+    };
+}
